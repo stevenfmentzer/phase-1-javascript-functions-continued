012b67f6298a65238234f7132943f231f3c087c8
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-13 17:45:25 -0500

    practice lab work

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    let response = 'This Saturday, I want to';
+    return (`${response} ${activity}!`)
+}
+
+function mondayWork(activity = 'go to the office'){
+    let response = 'This Monday, I will';
+    return (`${response} ${activity}.`)
+}
+
+function wrapAdjective(flair = "*"){ 
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
